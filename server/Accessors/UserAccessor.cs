6a0d335dbf40f2563555e9eaf0445c5e277d671e
Alexander Sanderson <asanderson5@huskers.unl.edu>
2026-04-12 20:20:26 -0500

    Initial logic to connect the server to the database
    This should get most of the way toward allowing other code
    (e.g. Program.cs) to call UserManager.GetUserType() to authenticate
    with the database and get actual data from it to display on a
    webpage. Although it compiles, two things should be noted:
    
    1. Some of the variables referenced in the SQL queries aren't
    accurate to the actual SQL we are using.
    
    2. This code still needs other code to call it for anything
    to actually happen.

---
+++ server/Accessors/UserAccessor.cs
@@ -0,0 +1,81 @@
+using server.IAccessors;
+using System.Data.SqlClient;
+
+namespace server.Accessors
+{
+    public class UserAccessor : IUserAccessor
+    {
+        public static SqlConnection GetConnection()
+        {
+            using (SqlConnection conn = new SqlConnection())
+            {
+                // TODO: Set the database IP as the program runs
+                conn.ConnectionString = "Data Source = 192.168.122.5;" +
+                "Initial Catalog=TestDB;" +
+                "User id=HUSKERSADMIN;" +
+                "Password=CORNADMIN;";
+                try
+                {
+                    conn.Open();
+                    return conn;
+                } catch (Exception es)
+                {
+                    return null;
+                }
+            }
+        }
+        public static bool Authenticate(string username, string password)
+        {
+            string sql = "SELECT user_id FROM USER_ACCOUNT WHERE username = @username AND password_hash = @password_hash";
+            using (SqlCommand cmd = new SqlCommand(sql, GetConnection()))
+            {
+                cmd.Parameters.Add("@username", System.Data.SqlDbType.NVarChar, 50);
+                cmd.Parameters.Add("@password_hash", System.Data.SqlDbType.NVarChar, 25);
+                cmd.Parameters["@username"].Value = username;
+                // TODO: hash password
+                cmd.Parameters["@password_hash"].Value = password;
+                try
+                {
+                    cmd.Connection.Open();
+                    cmd.ExecuteScalar();
+                    return true;
+                } catch (SqlException sx)
+                {
+                    Console.WriteLine("User not found!");
+                    return false;
+                }
+            }
+        }
+
+        public static string GetUserType(string username)
+        {
+            string sql = "SELECT type FROM USER_ACCOUNT WHERE username = @username";
+            using (SqlCommand cmd = new SqlCommand(sql, GetConnection()))
+            {
+                cmd.Parameters.Add("@username", System.Data.SqlDbType.NVarChar, 50);
+                cmd.Parameters["@username"].Value = username;
+                // TODO: hash password
+                try
+                {
+                    cmd.Connection.Open();
+                    string type = (string)cmd.ExecuteScalar();
+                    return type;
+                } catch (SqlException sx)
+                {
+                    Console.WriteLine("User not found!");
+                    return "Invalid";
+                }
+            }
+        }
+
+        public static User PullUser(string username)
+        {
+            return new User();
+        }
+
+        public static void PushUser(User user)
+        {
+            return;
+        }
+    }
+}
