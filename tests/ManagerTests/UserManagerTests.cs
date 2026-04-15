9503d3596178100c07093b14ed3287ce3d7f436e
bkib2005 <barrett.kiburz@gmail.com>
2026-04-15 12:30:23 -0500

    Added unit test structure and files for UserManager and UserAccessor.

---
+++ tests/ManagerTests/UserManagerTests.cs
@@ -0,0 +1,21 @@
+﻿namespace ManagerTests
+{
+    [TestClass]
+    public sealed class GetUserTypeTests
+    {
+        [TestMethod]
+        public void GetUserType_ReturnsAdmin()
+        {
+        }
+
+        [TestMethod]
+        public void GetUserType_ReturnsVoter()
+        {
+        }
+
+        [TestMethod]
+        public void GetUserType_ReturnsInvalid()
+        {
+        }
+    }
+}
