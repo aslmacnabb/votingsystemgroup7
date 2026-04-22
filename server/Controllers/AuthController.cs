d8fafc450862b9709b8e5c47b7ce951196292a3d
bkib2005 <barrett.kiburz@gmail.com>
2026-04-22 12:46:56 -0500

    Fixed problems with AuthController and IAuthController.

--- server/Controllers/AuthController.cs
+++ server/Controllers/AuthController.cs
@@ -3,12 +3,6 @@ namespace server.Controllers
 {
     public class AuthController : IAuthController
     {
-        private IAuthController AuthController;
-
-        public AuthController(IAuthController authController)
-        {
-            AuthController = authController;
-        }
 
     }
 }
