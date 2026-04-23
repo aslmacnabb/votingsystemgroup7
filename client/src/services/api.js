f03506de7b326bda6fce4181286cd966bd9213de
Anya MacNabb <aslmacnabb@gmail.com>
2026-04-23 15:24:12 -0500

    added blue accents and changed dummy data - amacnabb2

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,11 +4,7 @@ let ballots = [
     title: "Mayor Election",
     options: [
       { id: 1, text: "Issue 1" },
-      { id: 2, text: "Issue 2" },
-      { id: 3, text: "Issue 3" },
-      { id: 4, text: "Issue 4" },
-      { id: 5, text: "Issue 5" },
-      { id: 6, text: "Issue 6" }
+      { id: 2, text: "Issue 2" }
     ]
   },
   {
@@ -16,13 +12,17 @@ let ballots = [
     title: "City Council Election",
     options: [
       { id: 7, text: "Issue 7" },
-      { id: 8, text: "Issue 8" },
-      { id: 9, text: "Issue 9" },
-      { id: 10, text: "Issue 10" },
-      { id: 11, text: "Issue 11" },
-      { id: 12, text: "Issue 12" }
+      { id: 8, text: "Issue 8" }
     ]
-  }
+  },
+  {
+    id: 3,
+    title: "School Board Election",
+    options: [
+      { id: 9, text: "Issue 9" },
+    { id: 10, text: "Issue 10" }
+  ]
+ }
 ];
 
 let userVotes = [
