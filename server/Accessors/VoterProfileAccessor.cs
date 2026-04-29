1f09512e63c38ee209cadbabe8be4fc258300efd
bkib2005 <barrett.kiburz@gmail.com>
2026-04-29 12:12:36 -0500

    Added functions in VoterProfileAccessor to get and set the date and time a voter cast a a ballot and to set a voter's decision on a measure in the sql db.

--- server/Accessors/VoterProfileAccessor.cs
+++ server/Accessors/VoterProfileAccessor.cs
@@ -49,5 +49,23 @@ namespace server.Accessors
             string sql = GetSetterSqlString(data);
             GenericAccessor.SetDateTime(id, sql, new_value);
         }
+
+        public void SetMeasureDecision(int ballotId, string new_value)
+        {
+            string sql = "use VotingSystemDB; UPDATE Ballots SET SubmissionStatus = @new_value WHERE BallotId = @ballotId;";
+            GenericAccessor.SetString(ballotId, sql, new_value);
+        }
+
+        public DateTime GetBallotDate(int ballotId)
+        {
+            string sql = "use VotingSystemDB; SELECT CastAt FROM Ballots WHERE BallotId = @ballotId;";
+            return GenericAccessor.GetDateTime(ballotId, sql);
+        }
+
+        public void SetBallotDate(int ballotId, DateTime new_value)
+        {
+            string sql = "use VotingSystemDB; UPDATE Ballots SET CastAt = @new_value WHERE BallotId = @ballotId;";
+            GenericAccessor.SetDateTime(ballotId, sql, new_value);
+        }
     }
 }
